a8449ff1d55363c98fe8ccb84daa191afe4ce69c
Tim Harley <Timothy.D.Harley@gmail.com>
2019-04-09 19:53:27 -0500

    added scaffolding and create associate request

---
+++ ClinkedIn/Models/Associate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Models
+{
+    public class Associate
+    {
+        public Associate(int id, int userId, int associateId, string clinkType)
+        {
+
+        }
+    }
+}
