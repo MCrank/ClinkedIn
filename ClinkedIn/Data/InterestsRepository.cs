a10421c249bc2dc12ee7e1ae51918a66dcc9736a
unknown <jonathanmohan.net@gmail.com>
2019-04-09 19:56:55 -0500

    testing

---
+++ ClinkedIn/Data/InterestsRepository.cs
@@ -0,0 +1,24 @@
+﻿using ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Data
+{
+    public class InterestsRepository
+    {
+        static List<Interests> _interests = new List<Interests>();
+
+            public Interests AddInterest(string interest)
+            {
+                var newInterest = new Interests(interest);
+
+                newInterest.Id = _interests.Count + 1;
+
+                _interests.Add(newInterest);
+
+                return newInterest;
+            }
+        }
+    }
