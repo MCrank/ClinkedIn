69126ce0bd89b8c6d559ca6954912cf7b66194f0
Tim Harley <Timothy.D.Harley@gmail.com>
2019-04-10 16:16:59 -0500

    finished seed data

--- ClinkedIn/Data/AssociateRepository.cs
+++ ClinkedIn/Data/AssociateRepository.cs
@@ -30,6 +30,26 @@ namespace ClinkedIn.Data
             new Associate(18, 2, 13, "Enemy"),
             new Associate(19, 2, 3, "Friend"),
             new Associate(20, 2, 5, "Friend"),
+            new Associate(21, 3, 10, "Enemy"),
+            new Associate(22, 3, 9, "Enemy"),
+            new Associate(23, 3, 7, "Enemy"),
+            new Associate(24, 5, 14, "Enemy"),
+            new Associate(25, 5, 6, "Friend"),
+            new Associate(26, 5, 9, "Friend"),
+            new Associate(27, 6, 1, "Friend"),
+            new Associate(28, 6, 11, "Enemy"),
+            new Associate(29, 6, 15, "Enemy"),
+            new Associate(30, 7, 14, "Friend")
+        };
+
+        public Associate AddAssociate(int userId, int associateId, string clinkType)
+        {
+            var newAssociate = new Associate(userId, associateId, clinkType);
+            newAssociate.Id = _associates.Count + 1;
+            _associates.Add(newAssociate);
+            return newAssociate;
         }
+
+
     }
 }
