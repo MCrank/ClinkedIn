87f071310ff5e317061e87e2c8dba9b4df6312bb
MCrank <marco_mcse@hotmail.com>
2019-04-13 14:10:39 -0500

    Working Days Remaining

--- ClinkedIn/Data/UserRepository.cs
+++ ClinkedIn/Data/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ClinkedIn.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,7 +9,39 @@ namespace ClinkedIn.Data
     {
         static List<User> _users = new List<User>
         {
-            new User(1, "Jeremiah Ruggirello", "P@ssw0rd!", "Male", "Sweet Cakes", "Inmate", true),
+            /*
+             * 03/13/2017 08:05:34 PM
+             * 04/12/2017 10:06:50 PM
+             * 05/06/2017 07:40:48 AM
+             * 05/07/2017 07:52:50 AM
+             * 06/07/2017 01:37:27 PM
+             * 06/14/2017 11:16:50 PM
+             * 07/27/2017 08:25:57 AM
+             * 08/22/2017 09:21:07 PM
+             * 10/10/2017 03:27:22 PM
+             * 10/21/2017 08:11:49 PM
+             * 11/03/2017 07:22:44 AM
+             * 11/22/2017 06:19:52 AM
+             * 01/05/2018 09:00:11 AM
+             * 01/29/2018 07:45:39 PM
+             * 02/08/2018 07:36:00 PM
+             * 04/04/2018 03:08:49 AM
+             * 04/18/2018 07:32:02 PM
+             * 05/13/2018 12:52:43 AM
+             * 05/20/2018 10:28:31 PM
+             * 06/29/2018 05:53:07 AM
+             * 07/24/2018 06:24:20 AM
+             * 08/11/2018 11:20:31 AM
+             * 09/14/2018 10:39:05 PM
+             * 10/29/2018 02:40:44 PM
+             * 11/18/2018 09:46:30 PM
+             * 11/21/2018 11:30:33 AM
+             * 12/15/2018 05:52:02 PM
+             * 01/07/2019 11:52:26 PM
+             * 02/09/2019 02:59:17 AM
+             * 04/11/2019 09:38:00 AM
+             */
+            new User(1, "Jeremiah Ruggirello", "P@ssw0rd!", "Male", "Sweet Cakes", "Inmate", new DateTime()., new DateTime(, true),
             new User(2, "Varo Colombini", "P@ssw0rd!", "Female", "The Dove", "Inmate", true),
             new User(3, "Serenity Herrold", "P@ssw0rd!", "Female", "The Cat", "Inmate", false),
             new User(4, "Marco Crank", "P@ssw0rd!", "Male", "Two Toes", "Warden", true),
--- ClinkedIn/Models/User.cs
+++ ClinkedIn/Models/User.cs
@@ -9,16 +9,18 @@ namespace ClinkedIn.Models
     public class User
     {
 
-        public User(string name, string password, string gender, string nickName, string type)
+        public User(string name, string password, string gender, string nickName, DateTime startSentence, DateTime endSentence, string type)
         {
             Name = name;
             Password = password;
             Gender = gender;
             NickName = nickName;
+            StartSentence = startSentence;
+            EndSentence = endSentence;
             Type = type;
         }
 
-        public User(int id, string name, string password, string gender, string nickName, string type, bool active)
+        public User(int id, string name, string password, string gender, string nickName, string type, DateTime startSentence, DateTime endSentence, bool active)
         {
             Id = id;
             Name = name;
@@ -26,6 +28,8 @@ namespace ClinkedIn.Models
             Gender = gender;
             NickName = nickName;
             Type = type;
+            StartSentence = startSentence;
+            EndSentence = endSentence;
             ActiveMember = active;
         }
 
@@ -35,6 +39,17 @@ namespace ClinkedIn.Models
         public string Gender { get; set; }
         public string NickName { get; set; }
         public string Type { get; set; }
+        public DateTime StartSentence { get; set; }
+        public DateTime EndSentence { get; set; }
+
+        public int DaysRemaining
+        {
+            get
+            {
+                return (EndSentence - StartSentence).Days;
+            }
+        }
+
         public bool ActiveMember { get; set; }
     }
 }
